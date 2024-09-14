00abe39d493b5b7ab569bfcf3a21bb16ebd5b37c
IS-HAQ ABDUL-MOOMIN <114371157+abdul-moominis-haq@users.noreply.github.com>
2024-09-14 22:00:42 +0000

    Connecting Database

--- api/index.js
+++ api/index.js
@@ -1,27 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from '/routes/user.js';
 dotenv.config();
 
-
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO).then( () =>{
     console.log("Conneted to MongoDB successfully");
 })
 .catch( (err) =>{
     console.log(err);
-})
+});
 
 
 
 const app = express();
 
 app.listen(3000, () =>{
-    console.log('Server is running on port 3000 good');
+    console.log('Server is running on port 3000 good!!!');
 }
 );
-app.get('/test', (req, res) =>{
-    res.json({
-        message:'Hello World'
-    })
-} ) 
